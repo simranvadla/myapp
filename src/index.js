bde74b57039f23209c0dfe04e7f5ea5e8dd95687
npx <simran@lyrostech.com>
2024-03-18 17:25:01 +0530

    1

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import App5 from './App5';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <App5 />
   </React.StrictMode>
 );
 
